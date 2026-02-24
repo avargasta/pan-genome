949ce2a6fd0dd2b9ff47822c6c6b53e0fc1c944d
Niki Vazou <nikivazou@gmail.com>
2026-02-24 16:34:22 +0100

    test LH

--- src/FindIDRight.hs
+++ src/FindIDRight.hs
@@ -4,3 +4,8 @@ module FindIDRight where
 findIDRight :: [Int] -> [Int]
 findIDRight [] = []
 findIDRight (x:xs) = x : findIDRight xs
+
+
+{-@ foo :: {v:Int | v /= 0} -> Int @-}
+foo :: Int -> Int 
+foo x = 42 `div` x
